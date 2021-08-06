85e9c45649e0a7ffa1f6a4481f5282815af91046
Maroban <dbtmdqja207@naver.com>
2021-08-06 18:02:17 +0900

    08/06 controller, Dao, Service

--- src/main/java/com/javaex/controller/BoardController.java
+++ src/main/java/com/javaex/controller/BoardController.java
@@ -1,7 +1,7 @@
 package com.javaex.controller;
 
 import java.util.List;
-
+import java.util.Map;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,122 +21,136 @@ import com.javaex.vo.UserVo;
 @RequestMapping(value = "/board")
 public class BoardController {
 
-	// 필드
-	@Autowired
-	private BoardService boardService;
+  // 필드
+  @Autowired
+  private BoardService boardService;
 
-	// 생성자
+  // 생성자
 
-	// 메소드 - GS
+  // 메소드 - GS
 
-	// 메소드 - 일반
+  // 메소드 - 일반
 
-	/*** 글 읽기 ***/
-	@RequestMapping(value = "/read/{no}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String read(@PathVariable("no") int no, Model model) {
-		System.out.println("[현재 위치: BoardController.read]");
+  /*** 페이징 연습 리스트 ***/
+  @RequestMapping(value = "/list2", method = {RequestMethod.GET, RequestMethod.POST})
+  public String list2(Model model, @RequestParam(value = "page", required = false, defaultValue = "1") int page, @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword) {
+    System.out.println("[현재 위치: BoardController.list2]");
 
-		BoardVo boardVo = boardService.getBoard(no);
-		model.addAttribute("boardVo", boardVo);
+    Map<String, Object> listMap = boardService.getList2(page, keyword);
 
-		return "board/read";
-	}
+    model.addAttribute("listMap", listMap);
 
-	/*** 게시판 리스트 ***/
-	@RequestMapping(value = "/list", method = { RequestMethod.GET, RequestMethod.POST })
-	public String list(Model model, @RequestParam(required = false, defaultValue = "") String keyword) {
-		System.out.println("[현재 위치: BoardController.list]");
+    return "board/list2";
+  }
 
-		List<BoardVo> bList = boardService.selectList(keyword);
-		model.addAttribute("bList", bList);
 
-		return "board/list";
-	}
+  /*** 글 읽기 ***/
+  @RequestMapping(value = "/read/{no}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String read(@PathVariable("no") int no, Model model) {
+    System.out.println("[현재 위치: BoardController.read]");
 
-	/*** 게시글 작성 폼 ***/
-	@RequestMapping(value = "/writeForm", method = { RequestMethod.GET, RequestMethod.POST })
-	public String writeForm(HttpSession session) {
-		System.out.println("[현재 위치: BoardController.writeForm]");
+    BoardVo boardVo = boardService.getBoard(no);
+    model.addAttribute("boardVo", boardVo);
 
-		UserVo authUser = ((UserVo) session.getAttribute("authUser"));
+    return "board/read";
+  }
 
-		if (authUser != null) {
-			return "board/writeForm";
-		} else {
-			return "redirect:/user/loginForm";
-		}
+  /*** 게시판 리스트 ***/
+  @RequestMapping(value = "/list", method = {RequestMethod.GET, RequestMethod.POST})
+  public String list(Model model, @RequestParam(required = false, defaultValue = "") String keyword) {
+    System.out.println("[현재 위치: BoardController.list]");
 
-	}
+    List<BoardVo> bList = boardService.selectList(keyword);
+    model.addAttribute("bList", bList);
 
-	/*** 게시글 작성 ***/
-	@RequestMapping(value = "/write", method = { RequestMethod.GET, RequestMethod.POST })
-	public String write(@ModelAttribute BoardVo boardVo, HttpSession session) {
-		System.out.println("[현재 위치: BoardController.write]");
+    return "board/list";
+  }
 
-		UserVo authUser = ((UserVo) session.getAttribute("authUser"));
+  /*** 게시글 작성 폼 ***/
+  @RequestMapping(value = "/writeForm", method = {RequestMethod.GET, RequestMethod.POST})
+  public String writeForm(HttpSession session) {
+    System.out.println("[현재 위치: BoardController.writeForm]");
 
-		if (authUser != null) {
-			boardService.write(boardVo);
+    UserVo authUser = ((UserVo) session.getAttribute("authUser"));
 
-			return "redirect:/board/list";
-		} else {
-			return "redirect:/user/loginForm";
-		}
+    if (authUser != null) {
+      return "board/writeForm";
+    } else {
+      return "redirect:/user/loginForm";
+    }
 
-	}
+  }
 
-	/*** 게시글 수정 폼 ***/
-	@RequestMapping(value = "/modifyForm/{no}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String modifyForm(@PathVariable("no") int no, Model model, HttpSession session) {
+  /*** 게시글 작성 ***/
+  @RequestMapping(value = "/write", method = {RequestMethod.GET, RequestMethod.POST})
+  public String write(@ModelAttribute BoardVo boardVo, HttpSession session) {
+    System.out.println("[현재 위치: BoardController.write]");
 
-		UserVo authUser = ((UserVo) session.getAttribute("authUser"));
-		BoardVo boardVo = boardService.getOneBoard(no);
+    UserVo authUser = ((UserVo) session.getAttribute("authUser"));
 
-		if (authUser == null) {
-			return "redirect:/main";
-		} else {
+    if (authUser != null) {
+      boardService.write(boardVo);
 
-			if (authUser.getNo() == boardVo.getUser_no()) {
-				model.addAttribute("boardVo", boardVo);
+      return "redirect:/board/list";
+    } else {
 
-				return "board/modifyForm";
+      return "redirect:/user/loginForm";
+    }
 
-			} else {
-				return "redirect:/board/list";
+  }
 
-			}
-		}
+  /*** 게시글 수정 폼 ***/
+  @RequestMapping(value = "/modifyForm/{no}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String modifyForm(@PathVariable("no") int no, Model model, HttpSession session) {
 
-	}
+    UserVo authUser = ((UserVo) session.getAttribute("authUser"));
+    BoardVo boardVo = boardService.getOneBoard(no);
 
-	/*** 게시글 수정 ***/
-	@RequestMapping(value = "/modify", method = { RequestMethod.GET, RequestMethod.POST })
-	public String modify(@ModelAttribute BoardVo boardVo) {
+    if (authUser == null) {
+      return "redirect:/main";
+    } else {
 
-		boardService.modify(boardVo);
+      if (authUser.getNo() == boardVo.getUser_no()) {
+        model.addAttribute("boardVo", boardVo);
 
-		return "redirect:/board/list";
-	}
+        return "board/modifyForm";
 
-	/*** 게시글 삭제 ***/
-	@RequestMapping(value = "/delete/{no}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String delete(@PathVariable("no") int no, HttpSession session) {
+      } else {
+        return "redirect:/board/list";
 
-		UserVo authUser = ((UserVo) session.getAttribute("authUser"));
-		BoardVo boardVo = boardService.getOneBoard(no);
+      }
+    }
 
-		if (authUser == null) {
-			return "redirect:/main";
-		} else {
+  }
 
-			if (authUser.getNo() == boardVo.getUser_no()) {
-				boardService.delete(no);
+  /*** 게시글 수정 ***/
+  @RequestMapping(value = "/modify", method = {RequestMethod.GET, RequestMethod.POST})
+  public String modify(@ModelAttribute BoardVo boardVo) {
 
-			}
-			return "redirect:/board/list";
+    boardService.modify(boardVo);
 
-		}
+    return "redirect:/board/list";
+  }
 
-	}
+  /*** 게시글 삭제 ***/
+  @RequestMapping(value = "/delete/{no}", method = {RequestMethod.GET, RequestMethod.POST})
+  public String delete(@PathVariable("no") int no, HttpSession session) {
+
+    UserVo authUser = ((UserVo) session.getAttribute("authUser"));
+    BoardVo boardVo = boardService.getOneBoard(no);
+
+    if (authUser == null) {
+      return "redirect:/main";
+    } else {
+
+      if (authUser.getNo() == boardVo.getUser_no()) {
+        boardService.delete(no);
+
+      }
+      return "redirect:/board/list";
+
+    }
+
+  }
 
 }
